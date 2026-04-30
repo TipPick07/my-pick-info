2f2b78915e7f19855b29dc7cdc400c8c426dc57d
TipPick07 <TipPick07@users.noreply.github.com>
2026-04-30 17:30:01 +0900

    fix: 블로그 상세 페이지 카카오 배너 중복 제거

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -9,7 +9,6 @@ import { notFound } from "next/navigation";
 import fs from "fs";
 import path from "path";
 import AdBanner from "@/components/AdBanner";
-import KakaoChannelBanner from "@/components/KakaoChannelBanner";
 import CoupangBanner from "@/components/CoupangBanner";
 import EligibilityChecker from "@/components/EligibilityChecker";
 import SafeImage from "@/components/SafeImage";
@@ -304,7 +303,6 @@ export default async function BlogPostPage({ params }: PostPageProps) {
         </article>
 
         {/* Monetization Banners */}
-        <KakaoChannelBanner />
         <AdBanner />
         <CoupangBanner />
       </main>
